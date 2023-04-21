55c220c8febfc98d3680d964f8d60c979f2a15e4
nohchang <nchq2123@naver.com>
2023-04-21 19:18:26 +0900

    Aop 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -25,9 +25,9 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
